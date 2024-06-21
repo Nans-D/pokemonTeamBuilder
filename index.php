d84c3a45df9edd20ca1eecdd3f3ba96fa041cdae
Nans-D <ndelaubert@gmail.com>
2024-06-21 09:38:50 +0200

    Importation de guzzle / remise au propre de la page index

--- index.php
+++ index.php
@@ -1,48 +1,67 @@
 <?php
 
-use PokePHP\PokeApi;
-
-use function PokePHP\getFirstGeneration;
-
-include './vendor/danrovito/pokephp/src/PokeApi.php';
-
-$pokemonApi = new PokeApi;
-$pokemonFirstGeneration = getFirstGeneration("pokemon", "151");
-
-// Pagination
-$totalPokemon = count($pokemonFirstGeneration);
-$perPage = 8;
-$totalPages = ceil($totalPokemon / $perPage);
-
-$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-$page = max(1, min($page, $totalPages));
-
-$start = ($page - 1) * $perPage;
-
-$pokemonPage = array_slice($pokemonFirstGeneration, $start, $perPage);
 
 
-// tableaux
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise\Utils;
+
+set_time_limit(300); // Augmente la limite d'exécution à 300 secondes
+
+$client = new Client();
 $pokemonPhotos = [];
-$pokemonColor = [];
+$tempFile = 'pokemon_data.json';
 
-foreach ($pokemonPage as $pokemon) {
-    $pokemonResponse = $pokemonApi->pokemon($pokemon->name);
-    $pokemonPhotos[$pokemon->name] = json_decode($pokemonResponse)->sprites->other->{'official-artwork'}->front_default;
-    $pokemonColorEncode = $pokemonApi->pokemonSpecies($pokemon->name);
-    $pokemonColorString = json_decode($pokemonColorEncode);
-    $pokemonColor[$pokemon->name] = $pokemonColorString->color->name;
+// Vérifier si le fichier temporaire existe
+if (file_exists($tempFile)) {
+    // Lire les données depuis le fichier temporaire
+    $data = json_decode(file_get_contents($tempFile), true);
+
+    // Vérifier si les clés existent dans les données
+    if (isset($data['pokemonList']) && isset($data['pokemonPhotos'])) {
+        $pokemonList = $data['pokemonList'];
+        $pokemonPhotos = $data['pokemonPhotos'];
+    }
+} else {
+    // Récupérer les premiers 151 Pokémon en une seule requête
+    $response = $client->get('https://pokeapi.co/api/v2/pokemon?limit=151');
+    $pokemonList = json_decode(json_encode(json_decode($response->getBody())->results), true);
+
+    $promises = [];
+    foreach ($pokemonList as $pokemon) {
+        $url = $pokemon['url'];
+        $promises[$pokemon['name']] = $client->getAsync($url);
+    }
+
+    // Utilisation de Utils::settle pour gérer les promesses
+    $responses = Utils::settle($promises)->wait();
+
+    foreach ($responses as $name => $response) {
+        if ($response['state'] === 'fulfilled') {
+            $pokemonData = json_decode($response['value']->getBody());
+            $pokemonPhotos[$name] = $pokemonData->sprites->other->{'official-artwork'}->front_default;
+        } else {
+            // Gérer les erreurs si nécessaire
+            $pokemonPhotos[$name] = null;
+        }
+    }
+
+    // Enregistrer les données dans le fichier temporaire
+    file_put_contents($tempFile, json_encode([
+        'pokemonList' => $pokemonList,
+        'pokemonPhotos' => $pokemonPhotos
+    ]));
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pokémon Gallery</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -53,187 +72,65 @@ foreach ($pokemonPage as $pokemon) {
 
     <style>
         .pokemon-card {
-            width: 280px;
-            height: 350px;
-            padding: 13px;
+            margin: 10px;
+            text-align: center;
+            border: 1px solid #ddd;
             border-radius: 10px;
-            background-color: lightgrey;
-            box-shadow: 5px 5px 10px black;
-
-
+            padding: 10px;
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
         }
 
-        .pokemon-inside-card {
-            width: 100%;
-            height: 100%;
-            background: rgb(186, 186, 186);
-            background: linear-gradient(191deg, rgba(186, 186, 186, 1) 0%, rgba(237, 237, 237, 1) 18%, rgba(149, 149, 149, 1) 40%, rgba(245, 245, 245, 1) 60%, rgba(191, 191, 191, 1) 77%, rgba(122, 122, 122, 1) 100%);
-            box-shadow: inset 2px 2px 5px black;
-            display: flex;
-            flex-direction: column;
-        }
-
-
-        .shadow-filter-pokemon {
-            box-shadow: 0 0 10px black inset;
-        }
-
-        .card-pokemon:hover {
-            box-shadow: 5px 5px 10px #70B8FF;
-            cursor: pointer;
-
-        }
-
-        @media(min-width:1200px) {
-            .overflow-xl {
-                overflow: hidden;
-            }
+        .pokemon-img {
+            width: 100px;
+            height: 100px;
         }
     </style>
 </head>
 
-<body class="overflow-xl h-100" style="max-height:100vh;">
-    <section class="py-3" style="height:40vh;">
-        <div class="background-image"></div>
-        <div class="container-lg row justify-content-around mx-auto">
-            <div class="col align-self-center">
-                <img src="./assets/img/International_Pokémon_logo.svg.png" alt="" style="width:150px;">
-            </div>
-            <div class="col">
-                <ul class="row list-unstyled h-100">
-                    <li class="col text-center align-self-center">
-                        <a class="text-light text-decoration-none" href="#">Home</a>
-                    </li>
-                    <li class="col text-center align-self-center">
-                        <a class="text-light text-decoration-none" href="#">About</a>
-                    </li>
-                    <li class="col text-center align-self-center">
-                        <a class="text-light text-decoration-none" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col text-end align-self-center">
-                <button class="btn"><img src="./assets/img/pokeball.png" style="width:30px;" alt=""></button>
-            </div>
-        </div>
-    </section>
+<body class="overflow-xl" style="height:100vh;">
+    <?php include('header.php') ?>
 
-    <section class="container-fluid">
-        <div class="row align-items-center g-0">
-            <div class="col-4">
-                <div class="fs-1 text-light text-center w-100 text-bold">FILTER</div>
-                <ul class="list-unstyled row justify-content-center flex-column mx-auto">
-                    <li class="col-auto filter-button py-1 px-3 rounded-5 mx-auto my-3 text-center" style='background-color:#003362;box-shadow:5px 5px 10px #0D1520;'>
-                        <a class="text-light text-decoration-none fs-2" href="">Génération</a>
-                    </li>
-                    <li class=" col-auto filter-button py-1 px-3 rounded-5 mx-auto my-3 text-center" style='background-color:#003362;box-shadow:5px 5px 10px #0D1520;'>
-                        <a class="text-light text-decoration-none fs-2" href="">Color</a>
-                    </li>
-                    <li class=" col-auto filter-button py-1 px-3 rounded-5 mx-auto my-3 text-center" style='background-color:#003362;box-shadow:5px 5px 10px #0D1520;'>
-                        <a class="text-light text-decoration-none fs-2" href="">Evolution</a>
-                    </li>
-                </ul>
-
-
-                <nav class="mt-5">
-                    <ul class="row justify-content-center list-unstyled" style="gap: 10px;">
-                        <li class="col-auto li-pagination <?= ($page <= 1) ? 'disabled' : '' ?>">
-                            <a class="" href="?page=<?= $page - 1 ?>" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                            </a>
-                        </li>
-                        <?php for ($i = 1; $i <= 4; $i++) : ?>
-                            <li class="col-auto li-pagination <?= ($i == $page) ? 'active' : '' ?>">
-                                <a data-page="<?= $i ?>" class=" page-switch" type="button" href="?page=<?= $i ?>">
-                                    <?= $i ?>
-                                </a>
-                            </li>
-                        <?php endfor ?>
-                        <input id="input-pagination" type="text" class="col-auto li-pagination border border-none" name="" id="">
-                        <li class="col-auto li-pagination <?= ($page >= $totalPages) ? 'disabled' : '' ?>">
-                            <a class="" href="?page=<?= $page + 1 ?>" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                            </a>
-                        </li>
-                        <button id="button-pagination" data-page disabled class="col-auto btn btn-primary">Go</button>
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-8 position-relative">
-                <div id="spinner" class="d-none position-absolute top-50 start-50 translate-middle z-1">
-                    <img src="./assets/img/pokeball.png" alt="Pokéball" class="spin-pokeball" style="width:120px;">
-                </div>
-                <div id="printPokemonNewPage" class="row row-cols-4 gy-3 justify-content-center mx-auto align-content-center" style="height:60vh;">
-                    <?php foreach ($pokemonPage as $pokemon) { ?>
-
-                        <div class="col-3">
-                            <div class="position-relative rounded-5 card-pokemon" style="width:200px; height:250px;background: rgb(13,21,32);background: linear-gradient(331deg, rgba(13,21,32,1) 0%, rgba(0,51,98,1) 100%);">
-                                <div class="text-light text-center pt-2"><?= ucfirst($pokemon->name) ?></div>
-                                <div class="position-absolute top-50 start-50 translate-middle" style="height: 170px;width: 170px;background: rgb(13,21,32);background: radial-gradient(circle, rgba(13,21,32,1) 0%, rgba(16,77,135,1) 0%, rgba(13,21,32,1) 100%);border-radius: 50%;display: inline-block;border:2px solid <?= $pokemonColor[$pokemon->name]; ?>;">
-                                    <div class="position-absolute top-50 start-50 translate-middle">
-                                        <img class="object-fit-contain" style="width:100%;" src="<?= $pokemonPhotos[$pokemon->name] ?>" alt="">
-                                    </div>
-                                </div>
-                                <a id="modal" type="button" class="stretched-link" data-bs-toggle="modal" data-bs-target="#exampleModal"></a>
+    <div class="container-xl">
+        <section class="border border-danger my-2 p-4">
+            <div class="row justify-content-center">
+                <?php for ($i = 0; $i < 6; $i++) : ?>
+                    <div class="col-6 col-md-4 col-xl-2 d-flex justify-content-center align-items-center">
+                        <div class="rounded-5" style="aspect-ratio:2.80/4;height:250px;background: rgb(13,21,32);background: linear-gradient(331deg, rgba(13,21,32,1) 0%, rgba(0,51,98,1) 100%);">
+                            <div class="" style="height:80%;">
+                                <img data-card="<?= $i ?>" class="object-fit-contain build-card" style="width:100%;" src="" alt="">
                             </div>
-
+                            <div class="text-light text-center pt-2">Pikachu</div>
                         </div>
-                    <?php } ?>
-                </div>
+                    </div>
+                <?php endfor ?>
+            </div>
+        </section>
+        <div class="container">
+            <div class="row justify-content-center">
+                <?php foreach ($pokemonList as $pokemon) : ?>
+                    <?php if (isset($pokemonPhotos[$pokemon['name']])) : ?>
+
+                        <div class="col-auto pokemon-card" style="aspect-ratio:1/1;background: rgb(13,21,32);background: linear-gradient(331deg, rgba(13,21,32,1) 0%, rgba(0,51,98,1) 100%);" data-bs-toggle="popover" data-bs-content="<?php echo htmlspecialchars(ucfirst($pokemon['name'])); ?>" data-bs-placement="top">
+                            <img src="<?php echo htmlspecialchars($pokemonPhotos[$pokemon['name']]); ?>" alt="<?php echo htmlspecialchars($pokemon['name']); ?>" class="pokemon-img img-fluid">
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
         </div>
-    </section>
+    </div>
 
     <?php require_once('./modalPokemon.php') ?>
     <script>
         $(document).ready(function() {
-            $('.filter-button').on('mouseenter', function() {
-                $(this).addClass('shadow-filter-pokemon ')
-            })
-            $('.filter-button').on('mouseleave', function() {
-                $(this).removeClass('shadow-filter-pokemon ')
-            })
-
-
-            $(document).on('click', ".page-switch, #button-pagination", function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-
-                let page = $(this).data('page');
-                console.log($(this), page);
-
-                $.ajax({
-                    url: './api/getPokemonFirstGen.php',
-                    type: 'POST',
-                    data: `page=${page}`,
-                    beforeSend: function() {
-                        $('#spinner').removeClass('d-none');
-                        $('#printPokemonNewPage').addClass('blur-cards');
-                    },
-                    success: function(data) {
-                        if (data.response == 200) {
-                            $('#printPokemonNewPage').empty().append(data.html);
-                        }
-                    },
-                    complete: function() {
-                        $('#spinner').addClass('d-none');
-                        $('#printPokemonNewPage').removeClass('blur-cards');
-                    }
-                });
+            $('[data-bs-toggle="popover"]').popover({
+                trigger: 'hover'
             });
 
+            $('.pokemon-img').on('click', function() {
+                let src = $(this).attr('src')
+                $('[data-card="0"]').attr("src", src);
 
-            $('#input-pagination').on('input', function() {
-                let page = $(this).val();
-
-                if (page) {
-                    $('#button-pagination').prop('disabled', false).data('page', page);
-                } else {
-                    $('#button-pagination').prop('disabled', true);
-                }
-            });
-
-
+            })
         })
     </script>
 </body>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,15 +6,28 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit91e235ec69e60074af7b7ba70089ca5b
 {
+    public static $files = array (
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
             'Psr\\Log\\' => 8,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
             'PokePHP\\' => 8,
         ),
         'M' => 
         array (
             'Monolog\\' => 8,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
         ),
     );
 
@@ -23,6 +36,15 @@ class ComposerStaticInit91e235ec69e60074af7b7ba70089ca5b
         array (
             0 => __DIR__ . '/..' . '/psr/log/src',
         ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
         'PokePHP\\' => 
         array (
             0 => __DIR__ . '/..' . '/danrovito/pokephp/src',
@@ -30,6 +52,18 @@ class ComposerStaticInit91e235ec69e60074af7b7ba70089ca5b
         'Monolog\\' => 
         array (
             0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
     );
 
